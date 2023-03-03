4dc8f74a2a14ed05063040d7e45e951ed4d6b9d8
nadtosonna <prasolova.kateryna@gmail.com>
2023-03-03 17:07:12 +0200

    styles added

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -3,23 +3,31 @@ import logo from '../../images/logo.png';
 
 const CardItem = ({ id, avatar, name, tweets, followers, follow, toggleFollow }) => {
 
-const toggleFollowStatus = () => {toggleFollow(id)};
+  const toggleFollowStatus = () => {
+    toggleFollow(id);
+  };
+  const followersUI = () => {
+    return followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
 
   return (
     <li className={css.card}>
       <img src={logo} alt="logo" className={css.logo} />
       <div className={css.imgBlock}></div>
-      <div className={css.description}>
-        <img
+      <div className={css.avatarBlock}>
+        <div className={css.line}></div>
+        <div className={css.circle}>
+          <img
           src={avatar}
           alt="User avatar"
           className={css.avatar}
         />
+        </div>
       </div>
       <ul className={css.info}>
         <li className={css.stats}>{name}</li>
-        <li className={css.stats}>{tweets} tweets</li>
-        <li className={css.stats}>{followers} followers</li>
+        <li className={css.stats}>{tweets} {tweets.toString().endsWith('1') ? 'tweet' : 'tweets'}</li>
+        <li className={css.stats}>{followersUI()} {followers.toString().endsWith('1') ? 'follower' : 'followers'}</li>
       </ul>
       <button
         className={follow ? `${css.buttonActive}` : `${css.button}`}
